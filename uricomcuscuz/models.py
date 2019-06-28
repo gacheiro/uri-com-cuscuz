c7b32efa1b9714e52b35c4cdcc71dce091ff1554
thiagojobson <thiagojobson@gmail.com>
2019-06-28 17:47:32 -0300

    Corrigido um monte de erros

--- uricomcuscuz/models.py
+++ uricomcuscuz/models.py
@@ -2,29 +2,26 @@ from .uricomcuscuz import db
 
 
 class User(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
     submissions = db.relationship('Submission', 
-        backref=db.back_ref('user'), lazy=True)
+        backref=db.backref('user'), lazy=True)
 
     
 class Submission(db.Model):
-
-    id = id.Column(db.Integer, primary_key=True)
-    user_id = id.Column(db.Integer, db.ForeignKey('user.id'), 
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), 
         nullable=False)
-    problem_id = id.Column(db.Integer, db.ForeignKey('problem.id'), 
+    problem_id = db.Column(db.Integer, db.ForeignKey('problem.id'), 
         nullable=False)
     language = db.Column(db.String(), nullable=False)
-    ranking = id.Column(db.Integer, nullable=False)
-    exec_time = id.Column(db.Float, nullable=False)
-    date = id.Column(db.DateTime, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    exec_time = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
 
 
-class Problem(db):
-
-    id = id.Column(db.Integer, primary_key=True)
-    name = id.Column(db.String(), nullable=False)
+class Problem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
     submissions = db.relationship('Submission', 
-        backref=db.back_ref('problem'), lazy=True)
+        backref=db.backref('problem'), lazy=True)
