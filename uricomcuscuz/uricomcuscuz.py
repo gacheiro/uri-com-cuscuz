ad559beeda9fc97495b5c8fb4bf210f6fa1cd6bd
Thiago J. Barbalho <thiagojobson@gmail.com>
2019-11-24 19:15:59 -0300

    A raspagem de dados agora é um commando
    para executar: `flask fetch-subs`

--- uricomcuscuz/uricomcuscuz.py
+++ uricomcuscuz/uricomcuscuz.py
@@ -3,8 +3,10 @@ import datetime
 
 from flask import Flask, render_template, jsonify, request, g
 from flask_sqlalchemy import SQLAlchemy
+import asyncio
 
-from .scraping import profile_url_sorted, problem_url
+from uricomcuscuz.scraping import (profile_url_sorted, problem_url, 
+    fetch_all, parse_date)
 
 app = Flask(__name__) 
 
@@ -112,3 +114,34 @@ def utility_processor():
             return f'{date.day} {month_name(date.month)}'
 
     return dict(format_date=format_date)
+
+
+@app.cli.command('fetch-subs')
+def fetch_submissions():
+    """Faz a raspagem de dados no site do URI e atualiza o bd."""
+    print('Updating solutions...')
+    total_pages = int(app.config['TOTAL_PAGES'])
+    users, user_submissions = asyncio.run(fetch_all(total_pages))
+    for (user_id, user_name), submissions in zip(users, user_submissions):
+        user = User(id=user_id, name=user_name)
+        db.session.merge(user)
+        for sub in submissions:
+            (problem_id, 
+            problem_name, 
+            ranking, 
+            submission_id,
+            language, 
+            exec_time, 
+            date) = sub
+            problem = Problem(id=problem_id, name=problem_name)
+            submission = Submission(id=submission_id,
+                                    user_id=user_id,
+                                    problem_id=problem_id,
+                                    language=language,
+                                    ranking=ranking[:-1], # remove o º do final
+                                    exec_time=exec_time,
+                                    date=parse_date(date))
+            db.session.merge(problem)
+            db.session.merge(submission)            
+    db.session.commit()
+    print('done.')
